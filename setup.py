d11505e3789ee7e705dabb73bd78c0c8393bf97d
bxz9200 <zby.david@gmail.com>
2025-02-11 13:01:03 +0800

    first commit

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="Adversarial random forests (ARFs) recursively partition data into fully factorized leaves, where features are jointly independent. The procedure is iterative, with alternating rounds of generation and discrimination. Data become increasingly realistic at each round, until original and synthetic samples can no longer be reliably distinguished. This is useful for several unsupervised learning tasks, such as density estimation and generative modeling. Methods for both are implemented in this package.",
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    url='https://github.com/betterdataai/ts-metrics.git@pip',
+    url='https://github.com/bxz9200/bd-ts-metrics.git',
     license='MIT',
     install_requires=['numpy==1.18.5', 'matplotlib==3.5.3', 'scipy==1.7.3', 'dtaidistance==2.3.12', 'tensorflow==1.15.5', 'scikit-learn==1.0.2',
                       'torch==1.13.1+cu117', 'statsmodels==0.13.5', 'tslearn==0.6.3', 'seaborn==0.12.2', 'mgzip==0.2.1', 'protobuf==3.20.0'],
