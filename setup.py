bebc7fa41cea3d769264dff760ccc8f1b4d4be46
bxz9200 <zby.david@gmail.com>
2025-02-11 15:29:24 +0800

    update code

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="Betterdata time series metrics long description",
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    url='https://github.com/bxz9200/bd-ts-metrics.git',
+    url='https://github.com/bxz9200/bd-ts-metrics',
     license='MIT',
     install_requires=['numpy==1.18.5', 'matplotlib==3.5.3', 'scipy>=1.4', 'dtaidistance==2.3.12', 'tensorflow==1.15.5', 'scikit-learn==1.0.2',
                       'torch==1.13.1', 'statsmodels==0.13.5', 'tslearn==0.6.3', 'seaborn==0.12.2', 'mgzip==0.2.1', 'protobuf==3.20.0'],
