4687b2488fdaa6c9c0d801a925859428a0d39487
bxz9200 <zby.david@gmail.com>
2025-02-11 14:35:12 +0800

    update code

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     url='https://github.com/bxz9200/bd-ts-metrics.git',
     license='MIT',
     install_requires=['numpy>=1.20.3', 'matplotlib==3.5.3', 'scipy>=1.4', 'dtaidistance==2.3.12', 'tensorflow==1.15.5', 'scikit-learn==1.0.2',
-                      'torch==1.13.1+cu117', 'statsmodels==0.13.5', 'tslearn==0.6.3', 'seaborn==0.12.2', 'mgzip==0.2.1', 'protobuf==3.20.0'],
+                      'torch==1.13.1', 'statsmodels==0.13.5', 'tslearn==0.6.3', 'seaborn==0.12.2', 'mgzip==0.2.1', 'protobuf==3.20.0'],
 
     classifiers=["Programming Language :: Python :: 3.7",
                  "Programming Language :: Python :: 3.8",
