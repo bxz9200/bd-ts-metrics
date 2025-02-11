2b0cf1ffbdcf780a498d9df6c01904b38d47ff69
bxz9200 <zby.david@gmail.com>
2025-02-11 12:36:14 +0800

    update code for pip

---
+++ bd_ts_metrics.py
@@ -0,0 +1,72 @@
+import argparse
+import yaml
+import os
+import sys
+import json
+from src.preprocess import load_my_own_data_csv, load_my_own_data_numpy, load_my_own_data_csv_v2
+from src.evaluation import evaluate_data
+from src.utils import write_json_data
+
+class tsMetrics:
+    """Implements time series metrics in python
+      Usage:
+      1. evaluate the quality of synthetic time series data
+
+      :param config: metric config file.
+      :type config: str
+      :param real_data:  real time series data file
+      :type real_data: str
+      :param syn_data: synthetic time series data file
+      :type syn_data: str
+      :param seq_len: sequence length
+      :type seq_len: int
+    """
+
+    def __init__(self, config, real_data, syn_data, seq_len):
+      self.config = config
+      self.real_data = real_data
+      self.syn_data = syn_data
+      self.seq_len = seq_len
+
+
+    def load_config_from_file(self,config_file):
+        with open(config_file, 'r') as file:
+            return yaml.safe_load(file)
+
+
+    def evaluate(self):
+        config = self.load_config_from_file(self.config)
+        if self.real_data.endswith(".csv"):
+            data = load_my_own_data_csv(self.real_data, self.seq_len)
+        elif self.real_data.endswith(".npy"):
+            data = load_my_own_data_numpy(self.real_data)
+        else:
+            raise Exception("Real data: wrong data format")
+
+        if self.syn_data.endswith(".csv"):
+            generated_data = load_my_own_data_csv(self.syn_data, self.seq_len)
+        elif self.syn_data.endswith(".npy"):
+            generated_data = load_my_own_data_numpy(self.syn_data)
+        else:
+            raise Exception("Synthetic data: wrong data format")
+
+        if config['evaluation'].get('do_evaluation', True):
+            dataset_name = config['preprocessing'].get('dataset_name', 'dataset')
+            model_name = config['generation'].get('model', 'Betterdata-TS-Model')
+            if 'dataset_name' not in config['evaluation']:
+                config['evaluation']['evaluation'] = dataset_name
+            if 'model' not in config['evaluation']:
+                config['evaluation']['model'] = model_name
+            results = evaluate_data(config['evaluation'], data, generated_data)
+
+        if not os.path.isdir(os.path.join(os.getcwd(), 'result')):
+            os.mkdir(os.path.join(os.getcwd(), 'result'))
+
+        with open('./result/result.json', 'w') as f:
+            json.dump(results, f)
+
+        print('Program normal end.')
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="arfpy",
+    version="0.1.0",
+    author="Bingyin Zhao",
+    author_email="bingyin@betterdata.ai",
+    description="Betterdata time series metrics",
+    long_description="Adversarial random forests (ARFs) recursively partition data into fully factorized leaves, where features are jointly independent. The procedure is iterative, with alternating rounds of generation and discrimination. Data become increasingly realistic at each round, until original and synthetic samples can no longer be reliably distinguished. This is useful for several unsupervised learning tasks, such as density estimation and generative modeling. Methods for both are implemented in this package.",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url='https://github.com/betterdataai/ts-metrics/tree/pip',
+    license='MIT',
+    install_requires=['numpy==1.18.5', 'matplotlib==3.5.3', 'scipy==1.7.3', 'dtaidistance==2.3.12', 'tensorflow==1.15.5', 'scikit-learn==1.0.2',
+                      'torch==1.13.1+cu117', 'statsmodels==0.13.5', 'tslearn==0.6.3', 'seaborn==0.12.2', 'mgzip==0.2.1', 'protobuf==3.20.0'],
+
+    classifiers=["Programming Language :: Python :: 3.7",
+                 "Programming Language :: Python :: 3.8",
+                 "Programming Language :: Python :: 3.9",
+                 "Programming Language :: Python :: 3.10"]
+)
